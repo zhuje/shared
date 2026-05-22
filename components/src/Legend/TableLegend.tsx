8a78bde350683d0474cc692d9089cc661e07f1b5
Adrian <56292595+adrianSepiol@users.noreply.github.com>
2026-05-22 16:49:12 +0200

    [FEATURE] enable column resizing for the legend table (#125)
    Signed-off-by: Adrian Sepiół <adisepko@gmail.com>

--- components/src/Legend/TableLegend.tsx
+++ components/src/Legend/TableLegend.tsx
@@ -12,7 +12,7 @@
 // limitations under the License.
 
 import { ReactElement, useMemo } from 'react';
-import { Table, TableProps, TableColumnConfig } from '../Table';
+import { Table, TableProps, TableColumnConfig, DEFAULT_COLUMN_WIDTH, DEFAULT_COLUMN_MIN_WIDTH } from '../Table';
 import { LegendItem } from './legend-model';
 
 export interface TableLegendProps extends Pick<TableProps<LegendItem>, 'sorting' | 'onSortingChange'> {
@@ -31,6 +31,7 @@ const COLUMNS: Array<TableColumnConfig<LegendItem>> = [
     accessorKey: 'label',
     header: 'Name',
     enableSorting: true,
+    enableResizing: true,
 
     // Starting with `title` attr instead of a tooltip because it is easier to
     // implement. We should try adding a tooltip in the future, but we'll need
@@ -46,6 +47,9 @@ const getRowId: NonNullable<TableProps<LegendItem>['getRowId']> = (data) => {
 const getCheckboxColor: TableProps<LegendItem>['getCheckboxColor'] = (data) => {
   return data.color;
 };
+
+// This is a rough estimate of value that needs to be subtracted from the total width to avoid horizontal scrolling on initial render
+const TABLE_PADDING = 45;
 
 export function TableLegend({
   items,
@@ -75,8 +79,25 @@ export function TableLegend({
   }, [initRowSelection, items]);
 
   const columns = useMemo(() => {
-    return [...COLUMNS, ...additionalColumns];
-  }, [additionalColumns]);
+    // Calculate column widths to fit the container on initial render, avoiding unnecessary horizontal scrolling.
+    // Falls back to a default width if available width is too small; the user can resize columns as needed.
+    const additionalColumnsLength =
+      additionalColumns?.reduce((colWidth, col) => {
+        return colWidth + (typeof col.width === 'number' ? col.width : DEFAULT_COLUMN_WIDTH);
+      }, 0) ?? 0;
+    const availableWidth = Math.max(
+      width - additionalColumnsLength - TABLE_PADDING,
+      DEFAULT_COLUMN_MIN_WIDTH * COLUMNS.length
+    );
+    const columnWidth = availableWidth / COLUMNS.length;
+    return [
+      ...COLUMNS.map((column) => ({
+        ...column,
+        width: columnWidth,
+      })),
+      ...additionalColumns,
+    ];
+  }, [additionalColumns, width]);
 
   return (
     <Table
